7cf088f184082da72fb3751768eb70d0798fbe17
Oleksii Lavrov <lavovalexey@gmail.com>
2024-03-16 01:58:47 +0200

    test

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -7,10 +7,12 @@ import { TransactionsService } from './transactions.service';
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
+
   @Post()
   create(@Body() createTransactionDto: TransactionDto) {
     return this.transactionsService.add(createTransactionDto);
   }
+
   @Put(':id')
   update(
     @Param('id') id: string,
