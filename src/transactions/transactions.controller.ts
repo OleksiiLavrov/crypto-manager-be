d53a74318a708af8ffce99e96bfed9697c2ce78e
Oleksii Lavrov <lavovalexey@gmail.com>
2024-03-19 01:50:38 +0200

    feat(CRYP-13): add git hooks

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
 
 import { TransactionDto } from './dto/transaction.dto';
 
